40f67cf9136195a815afbcb8e0285f587eeaf323
suraiyasultana826 <suraiyasultana826@gmail.com>
2023-10-19 16:28:57 +0600

    added post

--- index.js
+++ index.js
@@ -33,6 +33,23 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const productCollection = client.db('productDB').collection('product');
+
+    app.get('/addProduct', async(req, res) => {
+      const cursor = productCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
+    app.post('/addProduct', async(req, res) => {
+      const newProduct = req.body;
+      console.log(newProduct);
+      const result = await productCollection.insertOne(newProduct);
+      res.send(result);
+    })
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
